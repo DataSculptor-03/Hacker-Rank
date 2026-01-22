eb6827cd99f024403e37564f4fb69db7308ce753
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2026-01-22 12:56:37 +0530

    Create Integers Come In All Sizes.py

---
+++ Integers Come In All Sizes.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+if((a>=1 and a<=1000)and(b>=1 and b<=1000)and(c>=1 and c<=1000)and(d>=1 and d<=1000)):
+    result=(a**b)+(c**d)
+print(result)
