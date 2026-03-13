6c629a3c66bdc13a86b2cdd2d9ee05137328c6af
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2026-03-13 12:15:34 +0530

    Create Electronics Shop.py

---
+++ Electronics Shop.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b):
+    #
+    # Write your code here.
+    #
+    s=0
+    l=[]
+    for i in range(0,len(keyboards)):
+        for j in range(0,len(drives)):
+            s=keyboards[i]+drives[j]
+            if(s<=b):
+                l.append(s)
+            s=0
+    if(len(l)>0):
+        return max(l)
+    else:
+        return -1
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
