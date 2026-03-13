3d32c283997ec2fd9fca45227557627a2f931e24
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2026-03-13 12:14:19 +0530

    Create Migratory Birds.py

---
+++ Migratory Birds.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    x=[0]*10
+    for i in arr:
+        x[i]+=1
+    m=max(x)
+    for i in range(0,len(x)):
+        if(x[i]==m):
+            return i
+            break
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
