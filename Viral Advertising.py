67763103fba228806342c9fd96b4a0cc57e75d12
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2026-04-05 13:32:27 +0530

    Create Viral Advertising.py

---
+++ Viral Advertising.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    if n==1:
+        return 2
+    elif n==2:
+        return 5
+    elif n==3:
+        return 9
+    elif n==4:
+        return 15
+    elif n==5:
+        return 24
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
